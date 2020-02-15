41fdb1b559f6f13fb439de70909b4d2ba8e9eb72
franlo <shigaloa@gmail.com>
2020-02-15 08:42:42 +0100

    Add support for triangles

--- src/lib/util/util.py
+++ src/lib/util/util.py
@@ -13,6 +13,8 @@ class Util(object):
             perimeter = reduce(lambda x, y: (2 * x) + (2 * y), args)
         elif shape == "circle":
             perimeter = 2 * math.pi * args[0]
+        elif shape == "triangle":
+            perimeter = reduce(lambda x, y: x+y, args)
         else:
             print "The shape, %s, is not supported" % (shape)
         return perimeter
